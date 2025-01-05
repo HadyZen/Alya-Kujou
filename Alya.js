7cfcebbfcf08bab30feb4066b9e86a407fe5e346
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2025-01-05 23:19:05 +0700

    Hady Zen

--- Alya.js
+++ Alya.js
@@ -47,7 +47,7 @@ console.log(logo.pesan + 'Mulai menerima pesan dari pengguna.');
    api.listenMqtt((err, event) => {
    const body = event.body;
 if (!body || maintain == true && !admin.includes(event.senderID) || chatdm == false && event.isGroup == false && !admin.includes(event.senderID)) return; 
-if (cdata(event.senderID) === 'gada') {
+if (!cdata() || cdata(event.senderID) === 'gada') {
   bdata(event.senderID, 'Unknown', 0);
 }
 if (body.toLowerCase() == "prefix") return api.sendMessage(`✨ Awalan ${nama} adalah: [ ${awalan} ]`, event.threadID, event.messageID);
