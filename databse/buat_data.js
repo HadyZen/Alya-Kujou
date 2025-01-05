3d477c2633bda97f220b3618a5622c58873cbaf4
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2025-01-05 22:32:11 +0700

    Hady zen

---
+++ databse/buat_data.js
@@ -0,0 +1,36 @@
+const sqlite = require('sqlite3').verbose();
+
+async function bdata(id, nama, yen) { 
+const db = new sqlite.Database('/database/alya.db', (err) => {
+  if (err) {
+    console.log(global.Alya.logo.error + 'Gagal terhubung ke database: ', err.message);
+  } else {
+    console.log(global.Alya.logo.info + 'Berhasil terhubung ke database.');
+  }
+});
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      data JSON
+    )
+  `);
+
+  const stmt = db.prepare('INSERT INTO users (data) VALUES (?)');
+  const data = JSON.stringify({ nama: nama, yen: yen });
+  stmt.run(id, data, function(err) {
+    if (err) {
+      console.log(global.Alya.logo.error + 'Gagal membuat data:', err.message);
+    } else {
+      console.log(global.Alya.logo.info + 'Berhasil menambahkan user.');
+    }
+  });
+
+  stmt.finalize();
+});
+
+db.close((err) => { });
+};
+
+module.exports = { bdata };
