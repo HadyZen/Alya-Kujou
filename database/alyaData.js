1547cfbe48181ea96c4a3b8ff8ddc7f4a0c2bc52
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2025-01-08 20:59:23 +0700

    Hady Zen

--- database/alyaData.js
+++ database/alyaData.js
@@ -1,22 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-let database = {};
+const database = fs.readFileSync(JSON.parse(path.join(__dirname, 'alyaData.json')), 'utf8');
+const kujou = {};
 
 function addData(id, nama, yen) {
     if (!database[id]) {
-        database[id] = { nama: nama, yen: yen };
+        kujou.push({ nama: nama, yen: yen });
         console.log(global.Alya.logo.data + 'Berhasil menambah pengguna baru.');
     }
     hady();
 }
 
 function hady() {
-  fs.writeFile(path.join(__dirname, 'alyaData.json'), JSON.stringify(database, null, 2), (err) => { });
+  fs.writeFile(path.join(__dirname, 'alyaData.json'), JSON.stringify(kujou, null, 2), (err) => { });
 }
 
 function getData(id) {
     if (database[id]) {
-        return database[id];
+        return JSON.parse(database[id]);
     } else {
         return null;
     }
@@ -25,7 +26,7 @@ function getData(id) {
 function setData(id, jenis, alya) {
     if (database[id]) {
         if (jenis === 'nama' || jenis === 'yen') {
-            database[id][jenis] = alya;
+            kujou[id][jenis] = alya;
             hady();
         } else {
             console.log(global.Alya.logo.error + 'Gagal mengganti data pengguna');
